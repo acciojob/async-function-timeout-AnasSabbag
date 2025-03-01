1d7be9c50d56c1569b66fff5533fa3fe8d1453bf
AnasSabbag <86312422+AnasSabbag@users.noreply.github.com>
2025-03-01 22:14:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+document.getElementById("btn").addEventListener("click",function () {
+  let tex = document.getElementById("text").value;
+	let delay = document.getElementById("delay").value;
+  let prom = new Promise ((resolve)=>{
+    setTimeout(()=>{
+      document.getElementById("output").innerHTML=`<p>${tex}</p>`
+      resolve("ok");
+    },delay*1000);
+  })
+
+	async function delayFunction(){
+		 await prom;
+    
+	}
+	
+	
+	delayFunction();
+  document.getElementById("text").value="";
+  document.getElementById("delay").value="";
+	
+})
+
