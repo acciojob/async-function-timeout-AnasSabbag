856b9bc00609cfe9d6d7aa9b032a68e6e9a50c95
AnasSabbag <86312422+AnasSabbag@users.noreply.github.com>
2025-03-02 10:24:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
-//your JS code here. If required.
-document.getElementById("btn").addEventListener("click",function () {
-  let tex = document.getElementById("text").value;
-	let delay = document.getElementById("delay").value;
-  let prom = new Promise ((resolve)=>{
-    setTimeout(()=>{
-      document.getElementById("output").innerHTML=`<p>${tex}-${delay}</p>`
-      resolve("ok");
-    },delay*1000);
-  })
 
-	async function delayFunction(){
-		 await prom;
+document.addEventListener("DOMContentLoaded",function() {
+	
+	let output= document.getElementById("output");
+
+  function delayMssg(dl) {
+		return new Promise((resolve)=>{
+			setTimeout(()=>{resolve()},dl*1000)
+    })
+	}
+
+	async function delayMessage(){
+
+    let text= document.getElementById("text").value;
+	  let delay= document.getElementById("delay").value;
+    document.getElementById("text").value="";
+	  document.getElementById("delay").value="";
+    if(text =="" || isNaN(delay) || delay<0){
+			return;
+		}
     
+		await delayMssg(delay);
+    
+		output.textContent =text;
+		
 	}
 	
+	document.getElementById("btn").addEventListener("click",delayMessage)
 	
-   delayFunction();
-  document.getElementById("text").value="";
-  document.getElementById("delay").value="";
 	
 })
-
